15fca848deee870d04987ad6b0942ab637c29ec1
Milena Aghabekyan <miaghabe@c2r1s2.42yerevan.am>
2025-12-22 13:44:38 +0400

    done

--- module05/ex01/main.cpp
+++ module05/ex01/main.cpp
@@ -1,19 +1,6 @@
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 
-// int main()
-// {
-// 	Bureaucrat bob("Bob", 100);
-// 	Bureaucrat chbob("ChBob", 1);
-
-// 	Form tax("Tax", 50, 20);
-
-// 	bob.signForm(tax);
-// 	chbob.signForm(tax);
-
-// 	return 0;
-// }
-
 int main()
 {
 	try {
